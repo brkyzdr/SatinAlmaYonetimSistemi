5b3cf7b20599bda9c71b56306c1cb45bf7e8b1d9
Berkay Ozder <berkayq3535@gmail.com>
2025-08-21 09:23:52 +0300

    Design Update

--- SatinAlmaYonetimSistemi/Forms/Suppliers.Designer.cs
+++ SatinAlmaYonetimSistemi/Forms/Suppliers.Designer.cs
@@ -98,6 +98,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button2);
