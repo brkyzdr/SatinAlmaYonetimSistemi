b1fb804adf3562b412bb0d54dd0d4c715edc21a7
Berkay Ozder <berkayq3535@gmail.com>
2025-08-27 15:09:01 +0300

    Forms Design Update

--- SatinAlmaYonetimSistemi/Forms/.Designer.cs
+++ SatinAlmaYonetimSistemi/Forms/.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SatinAlmaYonetimSistemi.Forms
 {
-    partial class Offers
+    partial class Invoices
     {
         /// <summary>
         /// Required designer variable.
@@ -34,19 +34,13 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonExit = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBoxItem = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBoxSuppliers = new System.Windows.Forms.ComboBox();
-            this.textBoxQuantity = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBoxUnit = new System.Windows.Forms.ComboBox();
-            this.textBoxPrice = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxItem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.comboBoxUnit = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -56,7 +50,7 @@
             // 
             this.buttonDelete.BackColor = System.Drawing.Color.LightCoral;
             this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonDelete.Location = new System.Drawing.Point(224, 17);
+            this.buttonDelete.Location = new System.Drawing.Point(224, 16);
             this.buttonDelete.Name = "buttonDelete";
             this.buttonDelete.Size = new System.Drawing.Size(68, 53);
             this.buttonDelete.TabIndex = 3;
@@ -68,7 +62,7 @@
             // 
             this.buttonUpdate.BackColor = System.Drawing.Color.LightSkyBlue;
             this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonUpdate.Location = new System.Drawing.Point(115, 17);
+            this.buttonUpdate.Location = new System.Drawing.Point(115, 16);
             this.buttonUpdate.Name = "buttonUpdate";
             this.buttonUpdate.Size = new System.Drawing.Size(68, 53);
             this.buttonUpdate.TabIndex = 4;
@@ -80,7 +74,7 @@
             // 
             this.buttonCreate.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.buttonCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonCreate.Location = new System.Drawing.Point(6, 17);
+            this.buttonCreate.Location = new System.Drawing.Point(6, 16);
             this.buttonCreate.Name = "buttonCreate";
             this.buttonCreate.Size = new System.Drawing.Size(68, 53);
             this.buttonCreate.TabIndex = 5;
@@ -98,7 +92,6 @@
             this.dataGridView1.Location = new System.Drawing.Point(311, 101);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.dataGridView1.Size = new System.Drawing.Size(877, 687);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
@@ -108,7 +101,7 @@
             this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonExit.BackColor = System.Drawing.Color.OldLace;
             this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonExit.Location = new System.Drawing.Point(1061, 17);
+            this.buttonExit.Location = new System.Drawing.Point(1061, 16);
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(109, 50);
             this.buttonExit.TabIndex = 9;
@@ -124,80 +117,53 @@
             this.groupBox1.Controls.Add(this.buttonExit);
             this.groupBox1.Controls.Add(this.buttonUpdate);
             this.groupBox1.Controls.Add(this.buttonDelete);
-            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(1176, 83);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             // 
-            // label1
+            // groupBox2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 16);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "Tedarikçi";
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBoxUnit);
+            this.groupBox2.Controls.Add(this.textBoxQuantity);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxItem);
+            this.groupBox2.Location = new System.Drawing.Point(12, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(293, 693);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
             // 
-            // label2
+            // textBoxItem
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 16);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Ürün";
-            // 
-            // comboBoxItem
-            // 
-            this.comboBoxItem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxItem.Enabled = false;
-            this.comboBoxItem.FormattingEnabled = true;
-            this.comboBoxItem.Location = new System.Drawing.Point(9, 92);
-            this.comboBoxItem.Name = "comboBoxItem";
-            this.comboBoxItem.Size = new System.Drawing.Size(278, 21);
-            this.comboBoxItem.TabIndex = 14;
+            this.textBoxItem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxItem.Location = new System.Drawing.Point(10, 32);
+            this.textBoxItem.Name = "textBoxItem";
+            this.textBoxItem.Size = new System.Drawing.Size(277, 20);
+            this.textBoxItem.TabIndex = 13;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(6, 130);
+            this.label3.Location = new System.Drawing.Point(6, 119);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 16);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "Birim";
-            // 
-            // comboBoxSuppliers
-            // 
-            this.comboBoxSuppliers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxSuppliers.FormattingEnabled = true;
-            this.comboBoxSuppliers.Location = new System.Drawing.Point(9, 35);
-            this.comboBoxSuppliers.Name = "comboBoxSuppliers";
-            this.comboBoxSuppliers.Size = new System.Drawing.Size(278, 21);
-            this.comboBoxSuppliers.TabIndex = 14;
-            this.comboBoxSuppliers.SelectedIndexChanged += new System.EventHandler(this.comboBoxSuppliers_SelectedIndexChanged);
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Miktar";
             // 
             // textBoxQuantity
             // 
             this.textBoxQuantity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxQuantity.Location = new System.Drawing.Point(9, 206);
+            this.textBoxQuantity.Location = new System.Drawing.Point(10, 135);
             this.textBoxQuantity.Name = "textBoxQuantity";
-            this.textBoxQuantity.Size = new System.Drawing.Size(278, 20);
-            this.textBoxQuantity.TabIndex = 11;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 187);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 16);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "Miktar";
+            this.textBoxQuantity.Size = new System.Drawing.Size(277, 20);
+            this.textBoxQuantity.TabIndex = 13;
             // 
             // comboBoxUnit
             // 
@@ -213,78 +179,32 @@
             "Cm",
             "Metrekare",
             "Metreküp"});
-            this.comboBoxUnit.Location = new System.Drawing.Point(9, 149);
+            this.comboBoxUnit.Location = new System.Drawing.Point(10, 83);
             this.comboBoxUnit.Name = "comboBoxUnit";
-            this.comboBoxUnit.Size = new System.Drawing.Size(278, 21);
-            this.comboBoxUnit.TabIndex = 14;
+            this.comboBoxUnit.Size = new System.Drawing.Size(277, 21);
+            this.comboBoxUnit.TabIndex = 12;
             // 
-            // textBoxPrice
+            // label2
             // 
-            this.textBoxPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxPrice.Location = new System.Drawing.Point(9, 261);
-            this.textBoxPrice.Name = "textBoxPrice";
-            this.textBoxPrice.Size = new System.Drawing.Size(278, 20);
-            this.textBoxPrice.TabIndex = 11;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Birim";
             // 
-            // label5
+            // label1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(6, 242);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 16);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Fiyat";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Ürün";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(6, 296);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 16);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "Para Birimi";
-            // 
-            // comboBoxCurrency
-            // 
-            this.comboBoxCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxCurrency.FormattingEnabled = true;
-            this.comboBoxCurrency.Items.AddRange(new object[] {
-            "TRY",
-            "USD",
-            "EUR",
-            "GBP",
-            "RUB",
-            "CNY"});
-            this.comboBoxCurrency.Location = new System.Drawing.Point(9, 315);
-            this.comboBoxCurrency.Name = "comboBoxCurrency";
-            this.comboBoxCurrency.Size = new System.Drawing.Size(278, 21);
-            this.comboBoxCurrency.TabIndex = 14;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.textBoxQuantity);
-            this.groupBox2.Controls.Add(this.comboBoxSuppliers);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.comboBoxCurrency);
-            this.groupBox2.Controls.Add(this.textBoxPrice);
-            this.groupBox2.Controls.Add(this.comboBoxUnit);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.comboBoxItem);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Location = new System.Drawing.Point(12, 96);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(293, 692);
-            this.groupBox2.TabIndex = 17;
-            this.groupBox2.TabStop = false;
-            // 
-            // Offers
+            // Invoices
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -294,10 +214,9 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Offers";
-            this.ShowIcon = false;
+            this.Name = "Invoices";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Offers";
+            this.Text = "Stocks";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
@@ -315,18 +234,12 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBoxItem;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBoxSuppliers;
-        private System.Windows.Forms.TextBox textBoxQuantity;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBoxUnit;
-        private System.Windows.Forms.TextBox textBoxPrice;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBoxCurrency;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxItem;
     }
 }
