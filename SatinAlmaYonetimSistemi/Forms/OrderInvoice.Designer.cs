55eaf5b64289a40076266a5f6f69ababd69030f1
Berkay Ozder <berkayq3535@gmail.com>
2025-09-02 15:08:12 +0300

    Order Invoice Form Added

---
+++ SatinAlmaYonetimSistemi/Forms/OrderInvoice.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SatinAlmaYonetimSistemi.Forms
+{
+    partial class OrderInvoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxInvoiceNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePickerInvoice = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxSupplier = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTotalAmount = new System.Windows.Forms.TextBox();
+            this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxTax = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fatura Bilgileri";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBoxCurrency);
+            this.groupBox1.Controls.Add(this.comboBoxSupplier);
+            this.groupBox1.Controls.Add(this.dateTimePickerInvoice);
+            this.groupBox1.Controls.Add(this.textBoxTax);
+            this.groupBox1.Controls.Add(this.textBoxTotalAmount);
+            this.groupBox1.Controls.Add(this.textBoxInvoiceNum);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(562, 651);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBoxInvoiceNum
+            // 
+            this.textBoxInvoiceNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInvoiceNum.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInvoiceNum.Location = new System.Drawing.Point(38, 144);
+            this.textBoxInvoiceNum.Name = "textBoxInvoiceNum";
+            this.textBoxInvoiceNum.Size = new System.Drawing.Size(482, 33);
+            this.textBoxInvoiceNum.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Fatura Numarası";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePickerInvoice
+            // 
+            this.dateTimePickerInvoice.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerInvoice.Location = new System.Drawing.Point(38, 224);
+            this.dateTimePickerInvoice.Name = "dateTimePickerInvoice";
+            this.dateTimePickerInvoice.Size = new System.Drawing.Size(482, 33);
+            this.dateTimePickerInvoice.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Fatura Tarihi";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxSupplier
+            // 
+            this.comboBoxSupplier.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSupplier.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxSupplier.FormattingEnabled = true;
+            this.comboBoxSupplier.Location = new System.Drawing.Point(38, 304);
+            this.comboBoxSupplier.Name = "comboBoxSupplier";
+            this.comboBoxSupplier.Size = new System.Drawing.Size(482, 33);
+            this.comboBoxSupplier.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tedarikçi";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Toplam Tutar";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxTotalAmount
+            // 
+            this.textBoxTotalAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxTotalAmount.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTotalAmount.Location = new System.Drawing.Point(38, 395);
+            this.textBoxTotalAmount.Name = "textBoxTotalAmount";
+            this.textBoxTotalAmount.Size = new System.Drawing.Size(335, 33);
+            this.textBoxTotalAmount.TabIndex = 1;
+            // 
+            // comboBoxCurrency
+            // 
+            this.comboBoxCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrency.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxCurrency.FormattingEnabled = true;
+            this.comboBoxCurrency.Items.AddRange(new object[] {
+            "TRY",
+            "USD",
+            "EUR",
+            "GBP",
+            "RUB",
+            "CNY"});
+            this.comboBoxCurrency.Location = new System.Drawing.Point(379, 395);
+            this.comboBoxCurrency.Name = "comboBoxCurrency";
+            this.comboBoxCurrency.Size = new System.Drawing.Size(141, 33);
+            this.comboBoxCurrency.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(33, 439);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Vergi Tutarı";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxTax
+            // 
+            this.textBoxTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxTax.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTax.Location = new System.Drawing.Point(38, 464);
+            this.textBoxTax.Name = "textBoxTax";
+            this.textBoxTax.Size = new System.Drawing.Size(482, 33);
+            this.textBoxTax.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(153, 548);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 59);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(459, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 59);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ÇIK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OrderInvoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(586, 675);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "OrderInvoice";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OrderInvoice";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxInvoiceNum;
+        private System.Windows.Forms.ComboBox comboBoxSupplier;
+        private System.Windows.Forms.DateTimePicker dateTimePickerInvoice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxCurrency;
+        private System.Windows.Forms.TextBox textBoxTotalAmount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTax;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ SatinAlmaYonetimSistemi/Forms/OrderInvoice.cs
@@ -0,0 +1,103 @@
+﻿using Data;
+using Data.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SatinAlmaYonetimSistemi.Forms
+{
+    public partial class OrderInvoice : Form
+    {
+        public OrderInvoice()
+        {
+            InitializeComponent();
+            ReadData();
+            FillComboBox();
+        }
+
+        private void ReadData()
+        {
+            DataTable dt = CRUD.Read
+                ("SELECT " +
+                "i.InvoiceNumber," +
+                "i.ID," +
+                "i.InvoiceDate," +
+                "s.Name AS SupplierName," +
+                "i.TotalAmount," +
+                "i.Currency," +
+                "i.TaxAmount," +
+                "i.CreatedDate," +
+                "i.CreatedBy " +
+                "FROM Invoices i " +
+                "INNER JOIN Suppliers s ON i.SupplierID = s.ID");
+
+            textBoxInvoiceNum.Text = dt.Rows[0]["InvoiceNumber"].ToString();
+            dateTimePickerInvoice.Text = dt.Rows[0]["InvoiceDate"].ToString();
+            comboBoxSupplier.Text = dt.Rows[0]["SupplierName"].ToString();
+            textBoxTotalAmount.Text = dt.Rows[0]["TotalAmount"].ToString();
+            comboBoxCurrency.Text = dt.Rows[0]["Currency"].ToString();
+            textBoxTax.Text = dt.Rows[0]["TaxAmount"].ToString();
+        }
+
+        private void SaveData()
+        {
+
+            DialogResult result = MessageBox.Show(
+                "Kaydı oluşturmak istediğinize emin misiniz?",   // Mesaj
+                "Onay",                                          // Başlık
+                MessageBoxButtons.YesNo,                         // Evet / Hayır butonları
+                MessageBoxIcon.Question                          // Soru ikonu
+            );
+
+            if (result == DialogResult.Yes)
+            {
+
+                var data = new Dictionary<string, object>
+                    {
+                        {"InvoiceNumber" ,textBoxInvoiceNum.Text },
+                        {"InvoiceDate" ,dateTimePickerInvoice.Value },
+                        {"SupplierID" ,comboBoxSupplier.SelectedValue },
+                        {"TotalAmount" ,textBoxTotalAmount.Text },
+                        {"Currency" ,comboBoxCurrency.Text },
+                        {"TaxAmount" ,textBoxTax.Text },
+                        {"CreatedDate" , DateTime.Now},
+                        {"CreatedBy" ,Session.UserID},
+                    };
+                CRUD.Create("Invoices", data);
+
+                MessageBox.Show("Kayıt başarıyla oluşturuldu.", "Bilgi");
+                ReadData();
+            }
+            else
+            {
+                MessageBox.Show("Kayıt işlemi iptal edildi.", "Bilgi");
+            }
+
+        }
+
+        private void FillComboBox()
+        {
+            DataTable supplierName = CRUD.Read("SELECT ID, Name FROM Suppliers ORDER BY Name");
+            comboBoxSupplier.DataSource = supplierName;
+            comboBoxSupplier.DisplayMember = "Name";
+            comboBoxSupplier.ValueMember = "ID";
+            comboBoxSupplier.SelectedItem = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
