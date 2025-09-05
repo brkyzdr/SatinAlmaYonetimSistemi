cb99efe58eae0ba8753a3ced03a2e52c94241704
Berkay Ozder <berkayq3535@gmail.com>
2025-09-05 14:12:25 +0300

    OffersAnswer Form Created

--- SatinAlmaYonetimSistemi/Forms/OffersAnswer.Designer.cs
+++ SatinAlmaYonetimSistemi/Forms/OffersAnswer.Designer.cs
@@ -28,49 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
             this.buttonSave = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxOffersAnswer = new System.Windows.Forms.ComboBox();
+            this.textBoxSupplier = new System.Windows.Forms.TextBox();
+            this.textBoxItem = new System.Windows.Forms.TextBox();
+            this.textBoxUnit = new System.Windows.Forms.TextBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxCurrency = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.Linen;
+            this.dataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.Linen;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(311, 101);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(877, 687);
-            this.dataGridView1.TabIndex = 6;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.SeaShell;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.SeaShell;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle11;
+            this.dataGridView.EnableHeadersVisualStyles = false;
+            this.dataGridView.GridColor = System.Drawing.Color.Gray;
+            this.dataGridView.Location = new System.Drawing.Point(344, 101);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.AntiqueWhite;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.ShowCellToolTips = false;
+            this.dataGridView.Size = new System.Drawing.Size(844, 687);
+            this.dataGridView.TabIndex = 6;
+            this.dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // buttonSave
             // 
             this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.buttonSave.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonSave.Location = new System.Drawing.Point(6, 635);
+            this.buttonSave.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(6, 628);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(281, 53);
+            this.buttonSave.Size = new System.Drawing.Size(314, 60);
             this.buttonSave.TabIndex = 7;
-            this.buttonSave.Text = "Kaydet";
+            this.buttonSave.Text = "Teklifi Onayla";
             this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // buttonExit
             // 
             this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonExit.BackColor = System.Drawing.Color.OldLace;
             this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonExit.Location = new System.Drawing.Point(1061, 19);
+            this.buttonExit.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(1069, 13);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(109, 50);
+            this.buttonExit.Size = new System.Drawing.Size(101, 60);
             this.buttonExit.TabIndex = 8;
             this.buttonExit.Text = "Geri";
             this.buttonExit.UseVisualStyleBackColor = false;
@@ -80,6 +132,8 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBoxOffersAnswer);
             this.groupBox1.Controls.Add(this.buttonExit);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
@@ -91,12 +145,115 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.textBoxCurrency);
+            this.groupBox2.Controls.Add(this.textBoxPrice);
+            this.groupBox2.Controls.Add(this.textBoxQuantity);
+            this.groupBox2.Controls.Add(this.textBoxUnit);
+            this.groupBox2.Controls.Add(this.textBoxItem);
+            this.groupBox2.Controls.Add(this.textBoxSupplier);
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.buttonSave);
             this.groupBox2.Location = new System.Drawing.Point(12, 94);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(293, 694);
+            this.groupBox2.Size = new System.Drawing.Size(326, 694);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Seçilen Teklif";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Satın Alma Teklifi:";
+            // 
+            // comboBoxOffersAnswer
+            // 
+            this.comboBoxOffersAnswer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOffersAnswer.Enabled = false;
+            this.comboBoxOffersAnswer.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxOffersAnswer.FormattingEnabled = true;
+            this.comboBoxOffersAnswer.Items.AddRange(new object[] {
+            "Onay Bekliyor",
+            "Onaylandı",
+            "Rededildi"});
+            this.comboBoxOffersAnswer.Location = new System.Drawing.Point(210, 27);
+            this.comboBoxOffersAnswer.Name = "comboBoxOffersAnswer";
+            this.comboBoxOffersAnswer.Size = new System.Drawing.Size(517, 33);
+            this.comboBoxOffersAnswer.TabIndex = 8;
+            // 
+            // textBoxSupplier
+            // 
+            this.textBoxSupplier.Enabled = false;
+            this.textBoxSupplier.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSupplier.Location = new System.Drawing.Point(6, 64);
+            this.textBoxSupplier.Name = "textBoxSupplier";
+            this.textBoxSupplier.Size = new System.Drawing.Size(314, 33);
+            this.textBoxSupplier.TabIndex = 10;
+            this.textBoxSupplier.Visible = false;
+            // 
+            // textBoxItem
+            // 
+            this.textBoxItem.Enabled = false;
+            this.textBoxItem.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxItem.Location = new System.Drawing.Point(6, 103);
+            this.textBoxItem.Name = "textBoxItem";
+            this.textBoxItem.Size = new System.Drawing.Size(314, 33);
+            this.textBoxItem.TabIndex = 10;
+            this.textBoxItem.Visible = false;
+            // 
+            // textBoxUnit
+            // 
+            this.textBoxUnit.Enabled = false;
+            this.textBoxUnit.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUnit.Location = new System.Drawing.Point(6, 142);
+            this.textBoxUnit.Name = "textBoxUnit";
+            this.textBoxUnit.Size = new System.Drawing.Size(314, 33);
+            this.textBoxUnit.TabIndex = 10;
+            this.textBoxUnit.Visible = false;
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Enabled = false;
+            this.textBoxQuantity.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQuantity.Location = new System.Drawing.Point(6, 181);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(314, 33);
+            this.textBoxQuantity.TabIndex = 10;
+            this.textBoxQuantity.Visible = false;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Enabled = false;
+            this.textBoxPrice.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPrice.Location = new System.Drawing.Point(6, 220);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(314, 33);
+            this.textBoxPrice.TabIndex = 10;
+            this.textBoxPrice.Visible = false;
+            // 
+            // textBoxCurrency
+            // 
+            this.textBoxCurrency.Enabled = false;
+            this.textBoxCurrency.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCurrency.Location = new System.Drawing.Point(6, 259);
+            this.textBoxCurrency.Name = "textBoxCurrency";
+            this.textBoxCurrency.Size = new System.Drawing.Size(314, 33);
+            this.textBoxCurrency.TabIndex = 10;
+            this.textBoxCurrency.Visible = false;
             // 
             // OffersAnswer
             // 
@@ -106,25 +263,36 @@
             this.ClientSize = new System.Drawing.Size(1200, 800);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "OffersAnswer";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Requests";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxOffersAnswer;
+        private System.Windows.Forms.TextBox textBoxCurrency;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.TextBox textBoxUnit;
+        private System.Windows.Forms.TextBox textBoxItem;
+        private System.Windows.Forms.TextBox textBoxSupplier;
     }
 }
--- SatinAlmaYonetimSistemi/Forms/OffersAnswer.cs
+++ SatinAlmaYonetimSistemi/Forms/OffersAnswer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using Data.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace SatinAlmaYonetimSistemi.Forms
 {
@@ -15,6 +18,68 @@ namespace SatinAlmaYonetimSistemi.Forms
         public OffersAnswer()
         {
             InitializeComponent();
+            ReadData();
+        }
+
+        private void ReadData()
+        {
+            DataTable dt = CRUD.Read(
+                "SELECT " +
+                "o.ID, s.Name AS SupplierName, o.Item, o.Quantity, o.Unit, o.Price, o.Currency, o.Date," +
+                "COALESCE(u.Name, '') + ' ' + COALESCE(u.Surname, '') + '(#' + CAST(u.ID AS varchar(20)) + ')' AS PurchaseName " +
+                "FROM Offers o " +
+                "INNER JOIN Suppliers s ON o.SupplierID = s.ID " +
+                "INNER JOIN Users u ON o.UserID = u.ID " +
+                "WHERE o.Status='Onay Bekliyor' ");
+
+            dataGridView.DataSource = dt;
+            dataGridView.EditMode = DataGridViewEditMode.EditOnEnter;
+            dataGridView.AutoGenerateColumns = true;
+
+            dataGridView.Columns["ID"].Visible = false;
+            dataGridView.Columns["SupplierName"].HeaderText = "Ürün";
+            dataGridView.Columns["Item"].HeaderText = "Ürün";
+            dataGridView.Columns["Unit"].HeaderText = "Birim";
+            dataGridView.Columns["Quantity"].HeaderText = "Miktar";
+            dataGridView.Columns["Price"].HeaderText = "Fiyat";
+            dataGridView.Columns["Currency"].HeaderText = "Para Birimi";
+            dataGridView.Columns["PurchaseName"].HeaderText = "Satınalma Sorumlusu";
+            dataGridView.Columns["Date"].HeaderText = "Teklif Tarihi";
+        }
+
+        private void SaveData()
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show(
+                "Kayıt etmek istediğinize emin misiniz?",   // Mesaj
+                "Onay",                                          // Başlık
+                MessageBoxButtons.YesNo,                         // Evet / Hayır butonları
+                MessageBoxIcon.Question                          // Soru ikonu
+                );
+
+
+                if (result == DialogResult.Yes)
+                {
+                    var data = new Dictionary<string, object>
+                    {
+                        {"ApprovedByID",Session.UserID },
+                        {"Status" , "Onaylandı"},
+                    };
+                    string condition = $"ID = '{dataGridView.SelectedRows[0].Cells["ID"].Value}'";
+                    CRUD.Update("Offers", data, condition);
+                    MessageBox.Show("Kayıt başarıyla yapıldı.", "Bilgi");
+                    ReadData();
+                }
+                else
+                {
+                    MessageBox.Show("İşlem iptal edildi.", "Bilgi");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen satır seçin.");
+            }
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
@@ -22,6 +87,42 @@ namespace SatinAlmaYonetimSistemi.Forms
             Dashboard dashboard = new Dashboard();
             dashboard.Show();
             this.Close();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            buttonSave.Enabled = true;
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView.Rows[e.RowIndex];
+                textBoxSupplier.Text = row.Cells["SupplierName"].Value.ToString();
+                textBoxItem.Text = row.Cells["Item"].Value.ToString();
+                textBoxQuantity.Text = row.Cells["Quantity"].Value.ToString();
+                textBoxUnit.Text = row.Cells["Unit"].Value.ToString();
+                textBoxPrice.Text = row.Cells["Price"].Value.ToString();
+                textBoxCurrency.Text = row.Cells["Currency"].Value.ToString();
+            }
+
+            textBoxSupplier.Enabled = true;
+            textBoxCurrency.Enabled = true;
+            textBoxItem.Enabled = true;
+            textBoxPrice.Enabled = true;
+            textBoxQuantity.Enabled = true;
+            textBoxUnit.Enabled = true;
+            label1.Enabled = true;
+
+            textBoxSupplier.Visible = true;
+            textBoxCurrency.Visible = true;
+            textBoxItem.Visible = true;
+            textBoxPrice.Visible = true;
+            textBoxQuantity.Visible = true;
+            textBoxUnit.Visible = true;
+            label1.Visible = true;
         }
     }
 }
